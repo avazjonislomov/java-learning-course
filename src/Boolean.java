5447e7a02175cfe611524f77d30dadab7f56b568
Avazjon Islomov <avazislamov04@gmail.com>
2026-09-25 22:53:11 +0500

    boolean type

--- src/Boolean.java
+++ src/Boolean.java
@@ -1,4 +1,18 @@
 public class Boolean {
+    boolean n1;
+    boolean n2;
 
 
-}
+
+
+    public  Boolean(){
+        n1 = true;
+        n2 = false;
+
+    }
+
+    @Override
+    public String toString(){
+        return "n1 = " + n1 + " , n2 = " + n2;
+    }
+    }
