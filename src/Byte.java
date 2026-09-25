30ea96b59631aadacdcc80fef92f35bccff13a98
Avazjon Islomov <avazislamov04@gmail.com>
2026-09-25 22:51:58 +0500

    byte type

--- src/Byte.java
+++ src/Byte.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Byte {
+        byte from;
+        byte to;
+
+        public Byte(){
+            from = -128 ;
+            to = 127;
+        }
+
+        @Override
+    public String toString(){
+            return "from: " + from + "; to: " + to;
+        }
 }
